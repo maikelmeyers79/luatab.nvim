86affd6442a7c3e3b02b4f1e011086832749fbee
maikelmeyers <github.maikz@xoxy.net>
2022-08-05 15:18:04 +0200

    use colon instead of brackets

--- lua/luatab/init.lua
+++ lua/luatab/init.lua
@@ -81,7 +81,7 @@ M.cell = function(index)
     local bufnr = buflist[winnr]
     local hl = (isSelected and '%#TabLineSel#' or '%#TabLine#')
 
-    return hl .. '%' .. index .. 'T' .. ' [' .. index .. ']' ..
+    return hl .. '%' .. index .. 'T' .. ' ' .. index .. ':' ..
         M.devicon(bufnr, isSelected) ..
         M.title(bufnr) ..
         M.modified(bufnr) .. '%T' ..
